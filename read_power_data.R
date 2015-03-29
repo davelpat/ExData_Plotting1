e29647dd84234878176924cb6f7927e7f20ad52b
davelpat <davelpat@gmail.com>
2015-03-28 17:05:28 -0700

    Added more explantory comments and reformatted the fread call for readability and maintainability

--- read_power_data.R
+++ read_power_data.R
@@ -17,8 +17,17 @@ read_power_data <- function(data_file, date_list) {
   if(!(file.exists(data_file))) {
     stop('Cannot find ', data_file)
   }
-  # Read in the requested data and replace the Date and Time variables
-  power_data <- tbl_df(fread(data_file, header = T, sep = ";", colClasses = rep("character", 9), nrows = 70000)) %>%
+  # Read in the requested data and replace the Date and Time variables.
+  # Only the first 70,000 of the data file are read because all the data we are 
+  # interested in for this project are in those lines; there is no need nor value
+  # in reading in the other 2 million lines. If later data is needed, that value
+  # would need to be updated or deleted.
+  # All the data is read in as type character to speed the read; otherwise, fread 
+  # would evaluate all the data as it is read in to guess the data type.
+  power_data <- tbl_df(fread(data_file, 
+                             nrows = 70000, 
+                             header = TRUE, sep = ";", 
+                             colClasses = rep("character", 9))) %>%
     filter(Date %in% date_list) %>%
     mutate(Date_time = dmy_hms(paste(Date, Time))) %>%
     mutate(Weekday = wday(Date_time,label = TRUE)) %>%
