6e812ff76b68e968a6dd6ed96530d1f5778cbb94
davelpat <davelpat@gmail.com>
2015-03-28 22:18:24 -0700

    Only source the read function and only read the data file if it is not already in the environment

--- plot1.R
+++ plot1.R
@@ -9,10 +9,14 @@ if(!require("lubridate")) {
   stop('Cannot find lubridate package')
 }
 
-# Read the data file
-source("read_power_data.R")
-feb_pwr <- read_power_data("household_power_consumption.txt", 
-                           c("1/2/2007", "2/2/2007"))
+# Read the data file, if we haven't already
+if(!"read_power_data" %in% ls()) {
+  source("read_power_data.R")
+}
+if(!"feb_pwr" %in% ls()) {
+  feb_pwr <- read_power_data("household_power_consumption.txt", 
+                             c("1/2/2007", "2/2/2007"))
+}
 
 # Open the png file
 png(filename = "plot1.png", width = 480, height = 480)
