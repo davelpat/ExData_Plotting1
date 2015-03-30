7590ed04cc4142473fc3a15dc61008fc60922364
davelpat <davelpat@gmail.com>
2015-03-29 21:23:26 -0700

    Removed the unneeded Weekday variable

--- read_power_data.R
+++ read_power_data.R
@@ -10,15 +10,15 @@ if(!require("lubridate")) {
 }
 
 # read_power_data takes the name of a data file and a list of dates to extract from it
-# then replaces the Date and Time variables with a Date_time instance and a weekday
-#name variable. The numerical data is coerce to type numeric.
+# then replaces the Date and Time variables with a Date_time instance.
+# The numerical data is coerced to type numeric.
 read_power_data <- function(data_file, date_list) {
   # Verify the data file exists
   if(!(file.exists(data_file))) {
     stop('Cannot find ', data_file)
   }
   # Read in the requested data and replace the Date and Time variables.
-  # Only the first 70,000 of the data file are read because all the data we are 
+  # Only the first 70,000 of the data file are read in because all the data we are 
   # interested in for this project are in those lines; there is no need nor value
   # in reading in the other 2 million lines. If later data is needed, that value
   # would need to be updated or deleted.
@@ -30,7 +30,6 @@ read_power_data <- function(data_file, date_list) {
                              colClasses = rep("character", 9))) %>%
     filter(Date %in% date_list) %>%
     mutate(Date_time = dmy_hms(paste(Date, Time))) %>%
-    mutate(Weekday = wday(Date_time,label = TRUE)) %>%
     select(Date_time, Weekday, (3:9))
 
   # Coerce the numerical data
